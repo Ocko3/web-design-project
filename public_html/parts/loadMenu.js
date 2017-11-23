3eb99d8b61a65766c737a5f13c8d6667ec1919b1
Olga Cinklová <oli.cinklova@gmail.com>
2017-11-23 15:06:37 +0100

    Pridano bio, music v progressu, news a shows pripraveno, mensi refactor

--- public_html/parts/loadMenu.js
+++ public_html/parts/loadMenu.js
@@ -1,12 +1,12 @@
 document.write('\
 \
-    <nav>\n' +
-    '    <a href="index.html" id="active">Home</a>\n' +
-    '    <a href="music.html">Music</a>\n' +
-    '    <a href="index.html">Bio</a>\n' +
-    '    <a href="index.html">Shows</a>\n' +
-    '    <a href="index.html">News</a>\n' +
-    '</nav>\
+    <nav><ul class="nav">\n' +
+    '    <li><a href="index.html" id="home" class="menu active">Home</a></li>\n' +
+    '    <li><a href="music.html" id="music" class="menu">Music</a></li>\n' +
+    '    <li><a href="bio.html" id="bio" class="menu">Bio</a></li>\n' +
+    '    <li><a href="shows.html" id="shows" class="menu">Shows</a></li>\n' +
+    '    <li><a href="news.html" id="news" class="menu">News</a></li>\n' +
+    '</ul></nav>\
 \
     <div id="button_container">\n' +
     '    <button id="button_contact" aria-label="dropdown_menu_button">\n' +
@@ -14,10 +14,11 @@ document.write('\
     '    <div id="dropdown_content">\n' +
     '        <a href="index.html">Home</a>\n' +
     '        <a href="music.html">Music</a>\n' +
-    '        <a href="index.html">Bio</a>\n' +
-    '        <a href="index.html">Shows</a>\n' +
-    '        <a href="index.html">News</a>\n' +
+    '        <a href="bio.html">Bio</a>\n' +
+    '        <a href="shows.html">Shows</a>\n' +
+    '        <a href="news.html">News</a>\n' +
     '    </div>\n' +
     '</div>\
 \
 ');
+
